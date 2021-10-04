e0d5f63c4f64081cc7424b7a284e7b08ad1110b3
iffrit51 <bryan.charles51@hotmail.fr>
2021-10-04 09:46:25 +0200

    Ajoutez des fichiers projet.

---
+++ ApplicationCore/WeatherForecast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationCore
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public int TerperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public string Summary { get; set; }
+
+    }
+}
